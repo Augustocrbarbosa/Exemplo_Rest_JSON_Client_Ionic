6954c049e35fb24b17b6a47da94f080ade011d19
Glauco <glauco.todesco@gmail.com>
2017-11-13 11:13:52 -0200

    Ionic Rest Sample

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { RestProvider } from '../../providers/rest/rest';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,18 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  clients: any;
+  
+  constructor(public navCtrl: NavController, public restProvider: RestProvider) {
+    this.getClients();
   }
 
+  
+  getClients() {
+    this.restProvider.getClients()
+    .then(data => {
+      this.clients = data;
+      console.log(this.clients);
+    });
+  }
 }
